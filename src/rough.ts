0e4703373db4c618710659b6ac13f82f82503760
Y.Petremann <y.petremann@gmail.com>
2021-09-26 14:27:47 +0200

    Update rough.ts

--- src/rough.ts
+++ src/rough.ts
@@ -1,11 +1,16 @@
 import { Config } from './core';
 import { RoughCanvas } from './canvas';
+import { RoughPaint } from './paint';
 import { RoughGenerator } from './generator';
 import { RoughSVG } from './svg';
 
 export default {
   canvas(canvas: HTMLCanvasElement, config?: Config): RoughCanvas {
     return new RoughCanvas(canvas, config);
+  },
+
+  paint(ctx: PaintRenderingContext2D, config?: Config): RoughCanvas {
+    return new RoughPaint(ctx, config);
   },
 
   svg(svg: SVGSVGElement, config?: Config): RoughSVG {
